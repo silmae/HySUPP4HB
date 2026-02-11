263e8e4127a5f05ad345a6ec40e3d6a1ee4ff99d
Kimmo <kimmo.a.riihiaho@jyu.fi>
2026-02-11 13:36:18 +0200

    Testing results looping

--- show_results.py
+++ show_results.py
@@ -4,6 +4,7 @@ import subprocess
 import datetime
 import sys
 import yaml
+import json
 
 from scipy.io import loadmat
 from matplotlib import pyplot as plt
@@ -14,66 +15,6 @@ from src.model.blind import MiSiCNet
 accepted_supervised_models = ["FCLS", "UnDIP"]
 accepted_blind_models = ["MiSiCNet", "MSNet"]
 accepted_extractors = ["SiVM", "SISAL", "VCA"]
-
-def show_results():
-
-    # runlist = [22]
-    runlist = range(50,77)
-
-    for run in runlist:
-        # print(run)
-        res = loadmat(f'{os.getcwd()}/logs/{run}/artifacts/Estimate/estimates.mat')
-        num_of_ems = res['E'].shape[1]
-        A = res['A']
-
-        # Min and max values for scaling the ab maps
-        vmin, vmax = 0, 1
-
-        # Setting the subplots size
-        cols = 3
-        if num_of_ems < cols:
-            rows = 1
-        else:
-            rows = int(np.ceil(num_of_ems / cols))
-        # print(num_of_ems, cols, rows)
-
-        fig, axes = plt.subplots(rows, cols, )
-        axes = axes.flatten()
-
-        i = 0
-        while i < num_of_ems:
-            # log_num = run.split('\\')[-1]
-
-            axes[i].set_title('Endmember: ' + str(i + 1))
-            im = axes[i].imshow(A[i, :, :], vmin=vmin, vmax=vmax)
-            # axes[i].axis('off')
-
-            i += 1
-
-        # Turn off axis of all the plots
-        for ax in axes:
-            ax.axis('off')
-
-        fig.tight_layout()
-        cbar = fig.colorbar(im, ax=axes, orientation='horizontal')  # , fraction=0.03, pad=0.05)
-        cbar.set_label("Abundance")
-        plt.show()
-
-        ####
-
-        plt.close("all")
-
-        for i, run in enumerate(runlist):
-            try:
-                E = res['E']
-                print(f"Endmember max: {np.max(E)}")
-                plt.plot(E)
-                # plt.set_title(f'Endmembers: {E.shape[-1]}')
-            except:
-                print(f'This run ({run}) probably did not finish...')
-
-        fig.tight_layout()
-        plt.show()
 
 
 def _init_logging():
@@ -201,6 +142,152 @@ def model_looper(themes, resolutions, models, extractors=None):
         else:
             experiment_looper(themes=themes, resolutions=resolutions, model_name=model)
 
+
+
+def by_key_val(dicts, keys: list, values: list):
+
+    if len(keys) != len(values):
+        raise ValueError(f"Provide as many keys {len(keys)} as values {len(values)}")
+
+    print(f"####### Results by {keys} == {values}  ################################")
+
+    for json_dict in dicts:
+        matches = True
+        for i, key in enumerate(keys):
+            if json_dict[key] != values[i]:
+                matches = False
+
+        if matches:
+            print(json_dict)
+
+
+def show_results():
+
+    path_dir_experiments = os.path.abspath(f"{os.getcwd()}/experiments/")
+    path_dir_supervised_results = os.path.join(path_dir_experiments, "supervised/")
+    path_dir_blind_results = os.path.join(path_dir_experiments, "blind/")
+
+    path_loop = path_dir_supervised_results
+    # path_loop = path_dir_blind_results
+
+    dicts = []
+
+    for dir_name in os.listdir(path_loop):
+        path_dir_result_top = os.path.abspath(os.path.join(path_loop, dir_name, "1/"))
+        split_name = dir_name.split("_")
+        extractor_name = split_name[-4]
+        unmixer_name = split_name[-3]
+        scene_name = split_name[-2]
+        resolution = split_name[-1]
+        # print(f"Scene {scene_name}, resolution {resolution}, path {path_dir_result_top}")
+        path_dir_metrics = os.path.join(path_dir_result_top, "metrics/")
+        path_file_sad = os.path.join(path_dir_metrics, "SAD.json")
+
+        if os.path.exists(path_file_sad):
+            with open(path_file_sad) as file:
+                json_dict = json.load(file)
+                json_dict["extractor_name"] = extractor_name
+                json_dict["unmixer_name"] = unmixer_name
+                json_dict["scene_name"] = scene_name
+                json_dict["resolution"] = resolution
+                json_dict["run_name"] = dir_name
+
+                # print(json_dict)
+                dicts.append(json_dict)
+
+    # by_key_val(dicts, keys=["extractor_name"], values=["SISAL"])
+    # by_key_val(dicts, keys=["extractor_name"], values=["SiVM"])
+    # by_key_val(dicts, keys=["unmixer_name"], values=["UnDIP"])
+    # by_key_val(dicts, keys=["unmixer_name"], values=["FCLS"])
+    reso = "64"
+    # by_key_val(dicts, keys=["scene_name", "resolution"], values=["FWP1", reso])
+    # by_key_val(dicts, keys=["scene_name", "resolution"], values=["FDS1", reso])
+    # by_key_val(dicts, keys=["scene_name", "resolution"], values=["FWP2", reso])
+    # by_key_val(dicts, keys=["scene_name", "resolution"], values=["FDS2", reso])
+    # by_key_val(dicts, keys=["scene_name", "resolution"], values=["IWP1", reso])
+    # by_key_val(dicts, keys=["scene_name", "resolution"], values=["IDS1", reso])
+    # by_key_val(dicts, keys=["scene_name", "resolution"], values=["OWP1", reso])
+    # by_key_val(dicts, keys=["scene_name", "resolution"], values=["ODS1", reso])
+    # by_key_val(dicts, keys=["scene_name", "resolution"], values=["OWP2", reso])
+    # by_key_val(dicts, keys=["scene_name", "resolution"], values=["ODS2", reso])
+
+    extract = "SISAL"
+    # by_key_val(dicts, keys=["scene_name", "extractor_name"], values=["ODS2", extract])
+
+    by_key_val(dicts, keys=["scene_name", "extractor_name"], values=["FWP1", extract])
+    by_key_val(dicts, keys=["scene_name", "extractor_name"], values=["FDS1", extract])
+    by_key_val(dicts, keys=["scene_name", "extractor_name"], values=["FWP2", extract])
+    by_key_val(dicts, keys=["scene_name", "extractor_name"], values=["FDS2", extract])
+    by_key_val(dicts, keys=["scene_name", "extractor_name"], values=["IWP1", extract])
+    by_key_val(dicts, keys=["scene_name", "extractor_name"], values=["IDS1", extract])
+    by_key_val(dicts, keys=["scene_name", "extractor_name"], values=["OWP1", extract])
+    by_key_val(dicts, keys=["scene_name", "extractor_name"], values=["ODS1", extract])
+    by_key_val(dicts, keys=["scene_name", "extractor_name"], values=["OWP2", extract])
+    by_key_val(dicts, keys=["scene_name", "extractor_name"], values=["ODS2", extract])
+
+
+
+    exit(0)
+
+    # runlist = [22]
+    runlist = range(50,77)
+
+    for run in runlist:
+        # print(run)
+        res = loadmat(f'{os.getcwd()}/logs/{run}/artifacts/Estimate/estimates.mat')
+        num_of_ems = res['E'].shape[1]
+        A = res['A']
+
+        # Min and max values for scaling the ab maps
+        vmin, vmax = 0, 1
+
+        # Setting the subplots size
+        cols = 3
+        if num_of_ems < cols:
+            rows = 1
+        else:
+            rows = int(np.ceil(num_of_ems / cols))
+        # print(num_of_ems, cols, rows)
+
+        fig, axes = plt.subplots(rows, cols, )
+        axes = axes.flatten()
+
+        i = 0
+        while i < num_of_ems:
+            # log_num = run.split('\\')[-1]
+
+            axes[i].set_title('Endmember: ' + str(i + 1))
+            im = axes[i].imshow(A[i, :, :], vmin=vmin, vmax=vmax)
+            # axes[i].axis('off')
+
+            i += 1
+
+        # Turn off axis of all the plots
+        for ax in axes:
+            ax.axis('off')
+
+        fig.tight_layout()
+        cbar = fig.colorbar(im, ax=axes, orientation='horizontal')  # , fraction=0.03, pad=0.05)
+        cbar.set_label("Abundance")
+        plt.show()
+
+        ####
+
+        plt.close("all")
+
+        for i, run in enumerate(runlist):
+            try:
+                E = res['E']
+                print(f"Endmember max: {np.max(E)}")
+                plt.plot(E)
+                # plt.set_title(f'Endmembers: {E.shape[-1]}')
+            except:
+                print(f'This run ({run}) probably did not finish...')
+
+        fig.tight_layout()
+        plt.show()
+
+
 if __name__ == "__main__":
 
     """
@@ -229,10 +316,10 @@ if __name__ == "__main__":
     themes = ["FWP1", "FDS1", "FWP2", "FDS2", "IWP1", "IDS1", "OWP1", "ODS1", "OWP2", "ODS2"]
     resolutions = [4, 16, 64, 256, 1024]
     # experiment_looper(themes=themes, resolutions=resolutions, extractor_name=extractor_name, model_name=model_name)
-    model_looper(themes=themes, resolutions=resolutions, extractors=accepted_extractors, models=accepted_supervised_models)
-    model_looper(themes=themes, resolutions=resolutions, models=accepted_blind_models)
+    # model_looper(themes=themes, resolutions=resolutions, extractors=accepted_extractors, models=accepted_supervised_models)
+    # model_looper(themes=themes, resolutions=resolutions, models=accepted_blind_models)
 
-    # show_results()
+    show_results()
 
 
 
